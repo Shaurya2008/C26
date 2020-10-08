91e7b83705022a7ccf0de8de21c9e45d1b4c1901
Shaurya2008 <mshaurya08@gmail.com>
2020-10-08 17:03:59 +0530

    changed Readme

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
+var backgroundImg,platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +14,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    
     
     ground = new Ground(600,height,1200,20)
 
@@ -22,7 +22,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    platform = new Ground(150,305,300,170)
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
